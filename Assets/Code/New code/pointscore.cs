0ea1194eda7257703271f575472ea8a7b03f2a05
turinthompson <71719803+turinthompson@users.noreply.github.com>
2021-03-31 09:09:41 -0700

    new art/ animations
    I fixed up the log, somewhat fixed the wood spike, added new frog sprites, added changed an old hurt animation and moved old player sprites into a folder called old player art

--- Assets/Code/New code/pointscore.cs
+++ Assets/Code/New code/pointscore.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
+﻿/**************
+ * by Ryan Scheppler
+ * date: 1/7/20
+ * desc add this to a object with collision, the player should be able to collide with this to get points
+ * 
+ * this is from the platformer game there are minor changes to make it work better
+ * ************/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class pointscore : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int points = 10;
+    private AudioSource myAud;
+
+    public UnityEvent OnDestory;
+
+    private void Start()
     {
-        
+        if (OnDestory == null)
+        {
+            OnDestory = new UnityEvent();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player") != false)
+        {
+            GameManager.Score += points;
+            OnDestory.Invoke();
+            Destroy(gameObject);
+        }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") != false)
+        {
+            GameManager.Score += points;
+            OnDestory.Invoke();
+            Destroy(gameObject);
+        }
+    }
+
 }

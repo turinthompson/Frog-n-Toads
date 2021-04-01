92cb7a9ff1567a3c5810a1e3aa570e52899d0e60
turinthompson <71719803+turinthompson@users.noreply.github.com>
2021-04-01 09:28:36 -0700

    added spme art, added a menu that needs to be finished

---
+++ Assets/Code/New code/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿/************
+ * Name: Ryan Scheppler
+ * Date: 1/4/2021
+ * Desc: Add this to an object over the actual pause menu objects to turn on and off
+ * 
+ * note this is from the platformer game
+ * **********/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuObjects;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        pauseMenuObjects.SetActive(true);
+    }
+    public void Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1.0f;
+        pauseMenuObjects.SetActive(false);
+    }
+}
---
+++ Assets/Code/New code/Settings.cs
@@ -0,0 +1,24 @@
+﻿/****************
+ * By: Ryan Shceppler
+ * date: 1/5/2021
+ * Desc: Functions to handle settingd
+ * **************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
